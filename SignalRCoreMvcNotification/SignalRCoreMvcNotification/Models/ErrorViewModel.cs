7776e95584b8de730cd149e825e2f3ae2d756c28
Sedat <sedatkocaer3734@hotmail.com>
2021-04-30 23:38:43 +0300

    Create project

---
+++ SignalRCoreMvcNotification/SignalRCoreMvcNotification/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRCoreMvcNotification.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
